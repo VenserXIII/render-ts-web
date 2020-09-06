6becba0d6ae5385268badfac2d69862899a041c4
FUEU91X <Extern.Daniel.Sausen@volkswagen.com.br>
2020-09-06 01:40:25 -0300

    Change routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import Home from './Home';
-import Links from './Links';
 
 import logo from './logo.svg';
 import './App.css';
@@ -12,18 +11,18 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
         <div className="App-links">
-          <Link to="/home">Home</Link>
-          <Link to="/links">Links</Link>
+          <Link to="/home">
+            <img src={logo} className="App-logo" width="100%" height="100%" alt="logo" />
+          </Link>
         </div>
-        <div></div>
-        <div></div>
-        <img src={logo} className="App-logo" width="100%" height="100%" alt="logo" />
         <div></div>
         <div></div>
         <div className="App-links">
           <Route exact path="/home" component={Home} />
-          <Route path="/links" component={Links} />
         </div>
         <div></div>
       </header>
--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <p>In the works...</p>
+      <a className="App-link" href="https://github.com/sausendaniel/render-ts-web" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <br />
+      <a className="App-link" href="https://codepen.io/Venser" target="_blank" rel="noopener noreferrer">CodePen</a>
     </>
   )
 }
