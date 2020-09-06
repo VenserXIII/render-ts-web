dff9a28217685f86b42ae49cb2908771144051d9
FUEU91X <Extern.Daniel.Sausen@volkswagen.com.br>
2020-09-05 23:50:11 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>In the works...</p>
+        <a className="App-link" href="https://codepen.io/Venser" target="_blank" rel="noopener noreferrer">CodePen</a>
+        <a className="App-link" href="https://github.com/VenserXIII" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
